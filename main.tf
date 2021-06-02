26a74c870da65ac5e6b6b80b460e471077bb2caa
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-06-01 17:06:17 -0700

    Chapter 3 Implementing Terraform Remote State with S3 - bucket creation

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc_module" {
-  source = "./modules/vpc_networking"
-  private_subnet_cidr = var.private_subnet_cidr
-  public_subnet_cidr = var.public_subnet_cidr
-  vpc_cidr_block = var.vpc_cidr_block
+resource "aws_s3_bucket" "terraform_remote_state_bucket" {
+  bucket = "tf-12-remote-states"
+  region = "us-west-2"
 }
